02c8ea04dce9f45435be79db9ec7ec882666ee44
Fabrig96 <fabriag9@gmail.com>
2022-09-06 14:39:11 -0300

    task pre finish

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import {HttpClient,HttpHandler } from '@angular/common/http';
+import {HttpClient,HttpHandler, HttpHeaders } from '@angular/common/http';
 import {Observable, of} from 'rxjs'
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +23,20 @@ export class TaskService {
     return this.http.get<Task[]>(this.apiUrl);
 
   }
+  
+  deleteTask(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskRemider(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+
+  }
+
+  addTask(task: Task): Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
+
 }
